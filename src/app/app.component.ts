b59d1517c43e8ab6f1da97e72cc2f4d0899b65ae
kirin97cesar <41177040+kirin97cesar@users.noreply.github.com>
2022-12-31 12:44:11 -0500

    Test complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  textBox: string;
+  textNumber: number;
+  commentArea: string;
+  controlSelector: string;
+  activeBtn: boolean = false;
+
+  constructor() {}
+
+  registerForm() {
+    console.log('el formulario es', this.activeBtn);
+    if (!this.activeBtn) {
+      return alert('Faltan completar los campos obligatorios!');
+    }
+    this.textBox = '';
+    this.textNumber = null;
+    this.commentArea = '';
+    this.controlSelector = '';
+    this.activeBtn = false;
+    alert('Enviado con exito!')
+  }
+
+  validarForm() {
+    console.log('se cambio');
+    if (this.textBox && this.textNumber && this.controlSelector) {
+      return (this.activeBtn = true);
+    }
+    return (this.activeBtn = false);
+  }
 }
